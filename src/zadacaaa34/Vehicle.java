52e83efb343457a4215ef230673ac8ea6c1be9fc
Jankulovska <jankulovskasara60@gmail.com>
2023-12-12 13:50:08 +0100

    Add first java program

---
+++ src/zadacaaa34/Vehicle.java
@@ -0,0 +1,18 @@
+package zadacaaa34;
+
+public class Vehicle {
+	private String color;
+
+    // Constructor with a default color
+    Vehicle() {
+        color = "Blue"; // Set a default color, e.g., "Blue"
+    }
+
+    // Constructor with a parameter to set the color
+    Vehicle(String a) {
+        color = a;
+    }
+public String toString() {
+	return color;
+}
+}
